1176fbbde0388183f5341e5e5a0fe783d74d24d3
q-pi <qpicard@hotmail.com>
2020-09-09 09:06:09 +0200

    debut ut jour2

--- api/api.py
+++ api/api.py
@@ -1,19 +1,24 @@
 from flask import *
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
 	return "<a href=\"" + url_for("book_hub") + "\" >Book</a>" + \
 		"<br></br><a href=\"" + url_for("books") + "\" >Books</a>"
 
+
 #@app.route('/api/books')
 #def books():
 #    return send_from_directory("data", "books.json", as_attachment=True)
 
+
 @app.route('/api/books')
 def books():
 	return jsonify(json.load(open("data/books.json")))
+
 
 @app.route('/api/book')
 def book_hub():
@@ -23,13 +28,16 @@ def book_hub():
 	    "<input type=\"submit\" value=\"Valider\">" + \
 		"</form>"
 
+
 @app.route('/api/book', methods=['POST'])
 def book_hub_post():
 	return redirect(url_for("book", id=request.form["book_id"]))
 
+
 @app.route('/api/book/<id>')
 def book(id):
 	return jsonify(json.load(open("data/books.json"))[int(id)])
+
 
 if __name__ == '__main__':
 	app.run()
